4397f334992de6006bb456e82e8d2dde99d40320
Charles <Charles@DESKTOP-EJCNUJN>
2018-03-01 18:45:46 +0000

    Added file generator method

--- GoogleHashCOF/src/me/cof/googlehash/Output.java
+++ GoogleHashCOF/src/me/cof/googlehash/Output.java
@@ -1,15 +1,28 @@
 package me.cof.googlehash;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Output {
-	private Vehicle[] vehicles;
+	//Attributes
+	List<Vehicle> vehicles = new ArrayList<Vehicle>();
+	private String filepath;
 	
-	public Output(Vehicle[] vehicles) {
+	//Constructor
+	public Output(ArrayList<Vehicle>vehicles, String filepath) {
 		this.vehicles = vehicles;
+		this.filepath = filepath;
 	}
 	
-	public File getFile() {
-		return File;
+	//Main file generator
+	public File generateFile() {
+		File f = new File(filepath);
+		
+		for (int i = 0; i > vehicles.size(); i++) {
+			
+		}
+		
+		return f;
 	}
 }
