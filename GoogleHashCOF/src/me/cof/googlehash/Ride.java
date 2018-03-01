92bbb4c380d9ac46e716a985d7c86083039bd823
chris burlacu <thereds9815@yahoo.com>
2018-03-01 18:20:37 +0000

    Ride class

---
+++ GoogleHashCOF/src/me/cof/googlehash/Ride.java
@@ -0,0 +1,37 @@
+package me.cof.googlehash;
+
+public class Ride {
+	
+	private int startTime; // Earliest start
+	private int finishTime; // Lastest finish
+	private int[] startLoc = new int[2]; // [start row, start column]
+	private int[] finishLoc = new int[2]; // [finish row, finish column]
+	
+	public Ride(int startTime, int finishTime, int[] startLoc, int[] finishLoc){ 
+		this.startTime = startTime;
+		this.finishTime = finishTime;
+		this.startLoc = startLoc;
+		this.finishLoc = finishLoc;
+		
+	}	
+	
+	public int getStartTime() {
+		return startTime;
+	}
+
+	public int getFinishTime() {
+		return finishTime;
+	}
+
+	public int[] getStartLoc() {
+		return startLoc;
+	}
+
+	public int[] getFinishLoc() {
+		return finishLoc;
+	}
+	
+
+}
+
+	
