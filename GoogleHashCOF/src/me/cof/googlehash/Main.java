d5b824c807b4a7e9873b46c69fa73073c42052f3
Charles <Charles@DESKTOP-EJCNUJN>
2018-03-01 16:38:17 +0000

    Added basic pizza loop

--- GoogleHashCOF/src/me/cof/googlehash/Main.java
+++ GoogleHashCOF/src/me/cof/googlehash/Main.java
@@ -3,8 +3,14 @@ package me.cof.googlehash;
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i <100; i++) {
-			System.out.print(i);
+		
+		Pizza pizza = new Pizza(null);
+		
+		for (int x = 0; x < pizza.getWidth() ; x++) {
+			for (int y = 0; y < pizza.getHeight() ; y++) {
+				
+				System.out.print(x + "-" + y);
+			}
 		}
 	}
 }
