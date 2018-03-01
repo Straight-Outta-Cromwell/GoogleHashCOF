6e18b1010a5b957d56efd83de1c3dae8bd34ac7b
Charles <Charles@DESKTOP-EJCNUJN>
2018-03-01 14:39:24 +0000

    Add pizza class.

---
+++ GoogleHashCOF/src/me/cof/googlehash/Pizza.java
@@ -0,0 +1,13 @@
+package me.cof.googlehash;
+
+public class Pizza {
+	private int slices;
+	
+	public Pizza(int slices) {
+		this.slices = slices;
+	}
+	
+	public int getSlicesNumber() {
+		return this.slices;
+	}
+}
