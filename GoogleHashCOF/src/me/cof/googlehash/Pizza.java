83cee8b14496546aa3262014b8c06fa20dd3f9a6
User <User@137.44.216.111>
2018-03-01 16:30:04 +0000

    Pizza Class Basic

--- GoogleHashCOF/src/me/cof/googlehash/Pizza.java
+++ GoogleHashCOF/src/me/cof/googlehash/Pizza.java
@@ -1,13 +1,16 @@
 package me.cof.googlehash;
 
 public class Pizza {
-	private int slices;
 	
-	public Pizza(int slices) {
-		this.slices = slices;
+	private int width;
+	private int height;
+	private int numIngr; // Amount of each, Tomato and Mushroom
+	private int maxSliceSize; // In cells
+	
+	private char[][] cells; // T - Tomato, M - Mushroom
+	
+	public Pizza(String filePath){
+		
 	}
 	
-	public int getSlicesNumber() {
-		return this.slices;
-	}
 }
