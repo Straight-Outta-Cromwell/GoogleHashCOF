704c9b0660202dad36c8cfe22c5c5f5f30b69c11
User <User@137.44.216.111>
2018-03-01 17:30:54 +0000

    jijij

--- GoogleHashCOF/src/me/cof/googlehash/ReadFile.java
+++ GoogleHashCOF/src/me/cof/googlehash/ReadFile.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class ReadFile {
 
-	public static void pizzaArray() {
+	public static char[][] pizzaArray(String filePath) {
 		Scanner file = null;
 		char[][] cells;
 
 		try {
-			file = new Scanner(new File("input/example.in"));
+			file = new Scanner(new File(filePath));
 		} catch (Exception e) {
 			System.exit(0);
 		}
@@ -31,40 +31,14 @@ public class ReadFile {
 			count++;
 		}
 
-		for (int w = 0; w < Integer.parseInt(properties[0]); w++) {
-			for (int h = 0; h < Integer.parseInt(properties[1]); h++) {
-				System.out.print(cells[w][h]);
-			}
-			System.out.print("\n");
-		}
-
-		// String st = in.nextLine();
-		// Scanner scan = new Scanner(st);
-		//
-		// int row = scan.nextInt();
-		//
-		// int column = scan.nextInt();
-		// int min = scan.nextInt();
-		//
-		// char[][] pizza = new char[row][column];
-		// String top = scan.next();
-		// int max = top.charAt(0);
-		//
-		//
-		// int count = 0;
-		// for(int r = 0; r < row; r++) {
-		// for(int c =0; c <column; c++) {
-		//
-		// pizza[r][c] = top.charAt(top.length() - 1);
-		// System.out.print(pizza[r][c]);
-		// count++;
-		//
-		// }
-		//
-		// }
-		// scan.close();
-		// in.close();
-		//
+//		for (int w = 0; w < Integer.parseInt(properties[0]); w++) {
+//			for (int h = 0; h < Integer.parseInt(properties[1]); h++) {
+//				System.out.print(cells[w][h]);
+//			}
+//			System.out.print("\n");
+//		}
+		
+		return cells;
 	}
 
 }
